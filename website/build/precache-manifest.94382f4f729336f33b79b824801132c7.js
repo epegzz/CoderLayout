0b75d00441644b692cdb3f6372bc90f7fba87eb6
Daniel <epegzz@gmail.com>
2020-01-03 14:30:46 +0100

    Include website build directory to serve with GitHub pages

---
+++ website/build/precache-manifest.94382f4f729336f33b79b824801132c7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "38e61974e69eb4549335",
+    "url": "/static/js/main.a157447a.chunk.js"
+  },
+  {
+    "revision": "ec89bb07cfdeff2127b5",
+    "url": "/static/js/2.c269c6e0.chunk.js"
+  },
+  {
+    "revision": "38e61974e69eb4549335",
+    "url": "/static/css/main.4e405b8a.chunk.css"
+  },
+  {
+    "revision": "95867236ec483c21a45de28b9bf15bc3",
+    "url": "/index.html"
+  }
+];
